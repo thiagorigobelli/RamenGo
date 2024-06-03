b23532fad2ec7712ac681d7d5ab50dac468e4909
Thiago Rigobelli <thirigobelli@hotmail.com>
2024-06-03 02:15:38 -0300

    Create pojo

---
+++ src/main/java/RedVenturesOrder.java
@@ -0,0 +1,13 @@
+
+public class RedVenturesOrder {
+
+	private String orderId;
+
+	public String getOrderId() {
+		return orderId;
+	}
+
+	public void setOrderId(String orderId) {
+		this.orderId = orderId;
+	}
+}
